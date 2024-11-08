73d436de9e83bbee96fa1ff6b11d3b11144d76f7
zhangyang08 <zhangyang08@qiyi.com>
2024-11-08 16:21:42 +0800

    day08 parse

--- AoC/2016/day08/main.zig
+++ AoC/2016/day08/main.zig
@@ -40,14 +40,177 @@
 const std = @import("std");
 const Regex = @import("regex").Regex;
 
+fn extract(alloc: std.mem.Allocator, comptime regex: []const u8, in: []const u8, T: anytype, comptime num: usize, output: *[num]T) !bool {
+    // std.debug.print("regex: {s}\n", .{regex});
+    // std.debug.print("in: {s}\n", .{in});
+    var re = try Regex.compile(alloc, regex);
+    const capOp = try re.captures(in);
+
+    if (capOp == null) {
+        return false;
+    }
+
+    // var params: [2]u32 = .{ 0, 0 };
+
+    const cap = capOp.?;
+
+    for (0..2) |i| {
+        const numOp = cap.sliceAt(i + 1);
+        if (numOp == null) {
+            continue;
+        }
+        // std.debug.print("column num: {s}\n", .{numOp.?});
+        output.*[i] = try std.fmt.parseInt(u32, numOp.?, 10);
+    }
+
+    return true;
+}
+
+const Rotate = union(enum) {
+    Row: [2]u32,
+    Column: [2]u32,
+
+    pub fn parse(alloc: std.mem.Allocator, in: []const u8) !?Rotate {
+        {
+            var re = try Regex.compile(alloc, "rotate column x=(\\d+) by (\\d+)");
+            const capOp = try re.captures(in);
+
+            if (capOp != null) {
+                var params: [2]u32 = .{ 0, 0 };
+
+                const cap = capOp.?;
+
+                for (0..2) |i| {
+                    const numOp = cap.sliceAt(i + 1);
+                    if (numOp == null) {
+                        continue;
+                    }
+                    // std.debug.print("column num: {s}\n", .{numOp.?});
+                    params[i] = try std.fmt.parseInt(u32, numOp.?, 10);
+                }
+
+                return Rotate{ .Column = params };
+            }
+        }
+
+        {
+            var re = try Regex.compile(alloc, "rotate row y=(\\d+) by (\\d+)");
+            const capOp = try re.captures(in);
+
+            if (capOp != null) {
+                var params: [2]u32 = .{ 0, 0 };
+
+                const cap = capOp.?;
+
+                for (0..2) |i| {
+                    const numOp = cap.sliceAt(i + 1);
+                    if (numOp == null) {
+                        continue;
+                    }
+                    params[i] = try std.fmt.parseInt(u32, numOp.?, 10);
+                }
+
+                return Rotate{ .Row = params };
+            }
+        }
+
+        return null;
+    }
+};
+
+const Instruction = union(enum) {
+    Rect: [2]u32,
+    Rotate: Rotate,
+
+    pub fn parse(alloc: std.mem.Allocator, in: []const u8) !?Instruction {
+        // var re = try Regex.compile(alloc, "rect (\\d+)x(\\d+)");
+        // const capOp = try re.captures(in);
+        //
+        // if (capOp != null) {
+        //     var params: [2]u32 = .{ 0, 0 };
+        //
+        //     const cap = capOp.?;
+        //
+        //     for (0..2) |i| {
+        //         const numOp = cap.sliceAt(i + 1);
+        //         if (numOp == null) {
+        //             continue;
+        //         }
+        //         // std.debug.print("rect num: {s}\n", .{numOp.?});
+        //         params[i] = try std.fmt.parseInt(u32, numOp.?, 10);
+        //     }
+        //
+        //     return Instruction{ .Rect = params };
+        // }
+        {
+            var params: [2]u32 = .{ 0, 0 };
+            const ok = try extract(alloc, "rect (\\d+)x(\\d+)", in, u32, 2, &params);
+            if (ok) {
+                return Instruction{ .Rect = params };
+            }
+        }
+
+        if (try Rotate.parse(alloc, in)) |v| {
+            return Instruction{ .Rotate = v };
+        }
+
+        return null;
+    }
+};
+
+fn tagUnion() void {
+    const instructions = [_]Instruction{
+        Instruction{ .Rect = .{ 1, 2 } },
+        Instruction{ .Rotate = Rotate{ .Row = .{ 3, 4 } } },
+        Instruction{ .Rotate = Rotate{ .Column = .{ 3, 4 } } },
+    };
+    for (instructions) |i| {
+        std.debug.print("cap: {?}\n", .{i});
+        switch (i) {
+            .Rect => |r| std.debug.print("cap: {any}\n", .{r}),
+            .Rotate => |r| switch (r) {
+                .Row => |v| std.debug.print("cap: {any}\n", .{v}),
+                .Column => |v| std.debug.print("cap: {any}\n", .{v}),
+            },
+        }
+    }
+}
+
+fn regexTest(alloc: std.mem.Allocator) !void {
+    var re = try Regex.compile(alloc, "rect (\\d+)x(\\d+)");
+    defer re.deinit();
+
+    const capOp = try re.captures("rect 3x2");
+
+    std.debug.print("cap? {}\n", .{capOp != null});
+
+    if (capOp == null) {
+        return;
+    }
+
+    const cap = capOp.?;
+
+    for (0..cap.len()) |i| {
+        const numOp = cap.sliceAt(i);
+        if (numOp == null) {
+            continue;
+        }
+        std.debug.print("cap: {s}\n", .{numOp.?});
+    }
+}
+
 pub fn main() !void {
-    std.debug.print("day07!\n", .{});
+    std.debug.print("day08!\n", .{});
+    tagUnion();
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
 
     const alloc = arena.allocator();
+
+    try regexTest(alloc);
 
     // const file = try (try std.fs.cwd().openDir("day04", .{})).openFile("test", .{});
     const file = try (try std.fs.cwd().openDir("day08", .{})).openFile("input", .{});
@@ -58,14 +221,11 @@ pub fn main() !void {
     const content = contentTmp[0 .. contentTmp.len - 1]; // vim keep last \n
     var lines = std.mem.split(u8, content, "\n");
 
+    var instrctions = std.ArrayList(Instruction).init(alloc);
     while (lines.next()) |l| {
         std.debug.print("line: {s}\n", .{l});
+        const instrction = (try Instruction.parse(alloc, l)).?;
+        std.debug.print("istruction: {any}\n", .{instrction});
+        try instrctions.append(instrction);
     }
-
-    var re = try Regex.compile(alloc, "rect (\\d+)x(\\d+)");
-    const cap = try re.captures("rect 3x2");
-
-    std.debug.print("cap? {}\n", .{cap != null});
-    // if (cap) |c| {
-    // }
 }
